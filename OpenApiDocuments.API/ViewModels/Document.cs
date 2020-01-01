b3375b940048c08312bb15efcaca2909a2ba855c
ozorOzora <ousghir.elias@gmail.com>
2020-01-01 14:37:06 +0100

    Added DocumentViewModel

---
+++ OpenApiDocuments.API/ViewModels/Document.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenApiDocuments.API.ViewModels
+{
+    public class DocumentViewModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("uid")]
+        public Guid UID { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
